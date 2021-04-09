ccddae49c7f6cfe42690e6522d7cf241145a09d1
mko2302 <mko2302@stacmail.net>
2021-04-09 15:19:37 +1200

    Create index.php

---
+++ website/index.php
@@ -0,0 +1,38 @@
+<?php
+  //includes database connection code
+  include("dbconnect.php");
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="custom.css">
+
+    <title>Cantamaths</title>
+  </head>
+  <body>
+    <!-- includes navbar code -->
+    <?php include("navbar.php"); ?>
+
+    <?php
+    // if user clicks on a link
+    if (isset($_GET['page'])) {
+      // opens page user clicked on
+      $page = $_GET['page'];
+      include("$page.php");
+      // else goes to home page
+    } else {
+      include("home.php");
+    }
+     ?>
+
+     <!-- Optional JavaScript -->
+     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
