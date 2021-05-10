5865870895eeeddf9ab9b8341af98fc793f929b0
mko2302 <mko2302@stacmail.net>
2021-05-11 09:34:03 +1200

    Update profile.php

--- website/profile.php
+++ website/profile.php
@@ -4,20 +4,15 @@
  include("userinfo.php");
 
 
-
-// if the users account is activated by admin
- if ($_SESSION['active']) {
-   echo "<p>Username: ";
-   echo $_SESSION['active'];
-   echo "</p>";
-   echo "<p>Status: Account Active</p>";
- } elseif ($_SESSION['inactive']) {
-   // if users account is not activated
-   echo "<p>Username: ";
-   echo $_SESSION['inactive'];
-   echo "</p>";
-   echo"<p>Status Account Inactive<p>";
+ if ($access == 1) {
+   $_SESSION['access'] = 'active';
+   echo "Active User";
+ } elseif ($access == 0) {
+   $_SESSION['access'] = 'inactive';
+   echo "Inactive User";
  }
+
+
 
  ?>
 
