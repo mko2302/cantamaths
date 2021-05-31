8d73426c32891cfaf69f7df1c774122cb7d96e55
mko2302 <mko2302@stacmail.net>
2021-06-01 09:27:16 +1200

    Update adminpanel.php

--- website/adminpanel.php
+++ website/adminpanel.php
@@ -8,32 +8,50 @@ if (!isset($_SESSION['admin'])) {
 }
  ?>
 
-<h2>Admin Panel</h2>
+<!-- change active tab on click -->
+ <script type="text/javascript">
+   $(document).ready(function() {
+   $('.admin-tab').click(function() {
+       $('.admin-tab').removeClass('active');
+       $(this).closest('.admin-tab').addClass('active')
+   });
+  });
+ </script>
 
-<div class="container">
-    <nav class="col-3">
-      <p class="display-4">Dashboard</p>
-      <a class="nav-link" href="">Home</a>
-      <a class="nav-link" href="">Add Questions</a>
-      <a class="nav-link" href="">Question Database</a>
-      <a class="nav-link" href="users.php"=>Users</a>
-      <a class="nav-link" href="logout.php">Log Out</a>
-    </nav>
+ <div class="my-4 row justify-content-center">
+   <div class="col-xl-2 m-2">
+     <nav class="nav nav-pills flex-column nav-stacked">
+       <p class="display-4">Dashboard</p>
+       <li class="nav-item">
+         <a class="nav-link admin-tab" href="index.php?page=adminpanel&tab=adminhome">Home</a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link admin-tab" href="index.php?page=adminpanel&tab=addquestion">Add Questions</a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link admin-tab" href="index.php?page=adminpanel&tab=questiondb">Question Database</a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link admin-tab" href="index.php?page=adminpanel&tab=users"=>Users</a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link admin-tab" href="logout.php">Log Out</a>
+       </li>
+     </nav>
+   </div>
 
-    <div class="col-9">
-      <p>Home Page</p>
 
-      <?php
-      if (isset($_GET[''])) {
-        // opens page user clicked on
-        $page = $_GET['page'];
-        include("$page.php");
-        // else goes to home page
-      } else {
-        include("home.php");
-      }
-      ?>
 
-    </div>
-
-</div>
+   <div class="col-xl-8">
+       <?php
+       if (isset($_GET['tab'])) {
+       //  opens page user clicked on
+        $tab = $_GET['tab'];
+         include("$tab.php");
+       //  else goes to home page
+       } else {
+         include("adminhome.php");
+       }
+       ?>
+   </div>
+ </div>
