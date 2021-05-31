2f89bc1f656376c76c84b5408caf7686ebf2a9a3
mko2302 <mko2302@stacmail.net>
2021-06-01 09:30:36 +1200

    Update navbar.php

--- website/navbar.php
+++ website/navbar.php
@@ -2,16 +2,21 @@
 
 <div class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="index.php"><h1>Cantamaths</h1></a>
+
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
+
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <?php
-        if(!isset($_SESSION['admin'])) {
+        if(!isset($_SESSION['user'])) {
           echo"<li class='nav-item'> <a class='nav-link' href='index.php?page=login'>Login</a> </li>";
-        } else {
+        } elseif (isset($_SESSION['admin'])) {
           echo"<li class='nav-item'> <a class='nav-link' href='index.php?page=adminpanel'>Admin Panel</a> </li>";
+          echo"<li class='nav-item'> <a class='nav-link' href='index.php?page=logout'>Logout</a> </li>";
+        } elseif (isset($_SESSION['user'])) {
+          echo"<li class='nav-item'> <a class='nav-link' href='index.php?page=profile'>Profile</a> </li>";
           echo"<li class='nav-item'> <a class='nav-link' href='index.php?page=logout'>Logout</a> </li>";
         };
        ?>
