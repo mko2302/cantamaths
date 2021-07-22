f7c96d3bbec428fa299d817a1de1b55c79a0fe84
mko2302 <mko2302@stacmail.net>
2021-07-22 16:56:28 +1200

    Update navbar.php
    fixed which nav items show up depending on user

--- website/navbar.php
+++ website/navbar.php
@@ -1,6 +1,5 @@
-<!-- link to home -->
-
 <div class="navbar navbar-expand-lg navbar-light bg-light">
+  <!-- link to home -->
   <a class="navbar-brand" href="index.php"><h1>Cantamaths</h1></a>
 
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -10,15 +9,20 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <?php
-        if(!isset($_SESSION['user'])) {
-          echo"<li class='nav-item'> <a class='nav-link' href='index.php?page=login'>Login</a> </li>";
-        } elseif (isset($_SESSION['admin'])) {
+        session_start();
+
+        // if user is an admin add admin panel button
+        if (isset($_SESSION['admin'])) {
           echo"<li class='nav-item'> <a class='nav-link' href='index.php?page=adminpanel'>Admin Panel</a> </li>";
-          echo"<li class='nav-item'> <a class='nav-link' href='index.php?page=logout'>Logout</a> </li>";
-        } elseif (isset($_SESSION['user'])) {
+        }
+
+        // if user logs in add items to navbar
+        if (isset($_SESSION['user'])) {
           echo"<li class='nav-item'> <a class='nav-link' href='index.php?page=profile'>Profile</a> </li>";
           echo"<li class='nav-item'> <a class='nav-link' href='index.php?page=logout'>Logout</a> </li>";
-        };
+        } else {
+          echo"<li class='nav-item'> <a class='nav-link' href='index.php?page=login'>Login</a> </li>";
+        }
        ?>
       </ul>
 
