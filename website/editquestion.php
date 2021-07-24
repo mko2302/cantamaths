75946dccd39f7fb4f24df7b2244fc2b4c67d236a
mko2302 <mko2302@stacmail.net>
2021-07-25 01:12:36 +1200

    Update editquestion.php
    main functionality working to edit, need to add error catching

--- website/editquestion.php
+++ website/editquestion.php
@@ -8,38 +8,55 @@
   if (isset($_POST['tag'])) {
     $tags = $_POST['tag'];
   }
-
+  // check if any questions in database are the same
   $check_sql = "SELECT * FROM question WHERE qnumber LIKE $qnumber AND yearID LIKE '$year' AND levelID LIKE $level AND answer LIKE $answer";
 
   $check_qry = mysqli_query($dbconnect, $check_sql);
 
-  foreach ($tags as $tag) {
-    $tag_check_sql = "SELECT * FROM questiontag WHERE questionID = $questionID AND $tagID LIKE $tag"
-  }
-
-  $check_tag_sql = "SELECT * FROM";
-
-  // clearstatcache();
+  // CHECK if user changed any tags
+  // foreach ($tags as $tag) {
+  //   $tag_check_sql = "SELECT * FROM questiontag WHERE questionID = $questionID AND $tagID LIKE $tag";
+  //   $tag_check_qry = mysqli_query($dbconnect, $check_sql);
+  //   $tag_check_aa = mysqli_fetch_assoc($tag_check_qry);
   //
-  //   if (mysqli_num_rows($check_qry) > 0){
-  //     header("Location:index.php?page=adminpanel&tab=questiondb&status=duplicateq");
-  //   } else {
-  //     // echo "$questionID $qnumber $answer $year $level ";
-  //     foreach ($tags as $tag) {
-  //       echo "$tag";
+  //   do {
+  //     $tagID_check = $tag_check_aa['tagID'];
+  //     if ($tagID_check = $tag) {
+  //
   //     }
+  //   } while $tag_check_aa = mysqli_fetch_assoc($tag_check_qry
   //
-  //     $update_sql = "UPDATE question
-  //                    SET qnumber = $qnumber, answer = $answer, levelID = $level, yearID = $year
-  //                    WHERE questionID = $questionID";
-  //
-  //     $update_qry = mysqli_query($dbconnect, $update_sql);
-  //
-  //     $questiontag_sql = "SELECT * FROM questiontag WHERE questionID = $questionID";
-  //     $questiontag_qry = mysqli_query($dbconnect, $questiontag_sql);
-  //     $questiontag_aa = mysqli_fetch_assoc($question_qry);
-  //
-  //
-  //   }
+  // }
+
+  clearstatcache();
+
+  if ((mysqli_num_rows($check_qry) > 0)){
+    header("Location:index.php?page=adminpanel&tab=questiondb&status=duplicateq");
+  } else {
+    //deletes previous tags
+    $delete_tag_sql = "DELETE FROM questiontag WHERE questionID=$questionID";
+    // sends sql query to data base
+    $delete_tag_qry = mysqli_query($dbconnect, $delete_tag_sql);
+
+    //adds new array of tags
+    if (isset($_POST['tag'])) {
+      foreach ($tags as $tagID){
+        $addtag_sql = "INSERT INTO questiontag (questionID, tagID) VALUES ($questionID, $tagID)";
+
+        $addtag_qry = mysqli_query($dbconnect, $addtag_sql);
+      }
+    }
+
+    $update_sql = "UPDATE question
+                   SET qnumber = $qnumber, answer = $answer, levelID = $level, yearID = $year
+                   WHERE questionID = $questionID";
+
+    $update_qry = mysqli_query($dbconnect, $update_sql);
+
+    $questiontag_sql = "SELECT * FROM questiontag WHERE questionID = $questionID";
+    $questiontag_qry = mysqli_query($dbconnect, $questiontag_sql);
+    $questiontag_aa = mysqli_fetch_assoc($questiontag_qry);
+
+  }
 
  ?>
