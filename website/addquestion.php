f5305ed24dc97595d50f03be0d8f9c761d3cd830
mko2302 <mko2302@stacmail.net>
2021-08-09 10:27:45 +1200

    Update addquestion.php
    Formatting

--- website/addquestion.php
+++ website/addquestion.php
@@ -38,110 +38,124 @@ if(!isset($_GET['status'])) {
   ?>
   <!-- form for entering question -->
   <form action="index.php?page=adminpanel&tab=enterquestion" method="post" enctype="multipart/form-data">
-      <div class="row">
-        <div class="form-group col-lg-6">
-          <label class="form-label" for="fileToUpload">Upload Question Image</label> <br>
-          <input class="" type="file" name="fileToUpload" id="fileToUpload" accept="image/*" onchange="loadFile(event)">
-        </div>
-      </div>
-
-      <div class="row align-items-center">
-        <!-- enter qusetion number -->
-        <div class="form-group col-2">
-          <label for="qnumber">Question Number</label><br>
-          <input class="form-control" id="qnumber" name="qnumber" type="number" placeholder="Enter Number" min="1" max="20" required>
-          <script>
-            document.querySelector("input[type=number]")
-            .oninput = e => console.log(new Date(e.target.valueAsNumber, 0, 1))
-          </script>
-        </div>
-        <!-- script to load image when user upload -->
-        <div class="col-lg-2">
-          <img id="output" height="150" class="questionimg">
-          <script>
-            var loadFile = function(event) {
-              var output = document.getElementById('output');
-              output.src = URL.createObjectURL(event.target.files[0]);
-              output.onload = function() {
-                URL.revokeObjectURL(output.src) // free memory
-              }
-            };
-          </script>
+    <div class="row">
+      <div class="col-6">
+        <div class="row">
+          <div class="form-group col-lg-8">
+            <label class="form-label" for="fileToUpload">Upload Question Image</label> <br>
+            <input class="" type="file" name="fileToUpload" id="fileToUpload" accept="image/*" onchange="loadFile(event)">
+          </div>
         </div>
 
-        <!-- input for answer -->
-        <div class="form-group col-lg-2">
-          <label for="answer">Answer</label><br>
-          <input class="form-control" type="text" name="answer" placeholder="Enter Answer" required>
+        <div class="row">
+          <!-- script to load image when user upload -->
+          <div class="col">
+            <img id="output" height="150" class="questionimg">
+            <script>
+              var loadFile = function(event) {
+                var output = document.getElementById('output');
+                output.src = URL.createObjectURL(event.target.files[0]);
+                output.onload = function() {
+                  URL.revokeObjectURL(output.src) // free memory
+                }
+              };
+            </script>
+          </div>
         </div>
 
-        <!-- select/type year of publication -->
-        <div class="form-group col-lg-1.5">
-          <label for="year">Publication Year</label><br>
-          <select class="form-control" name="year" required>
-            <?php
-              $year_sql = "SELECT * FROM year ORDER BY yearID DESC";
-              $year_qry = mysqli_query($dbconnect, $year_sql);
-              $year_aa = mysqli_fetch_assoc($year_qry);
+        <div class="row">
+          <!-- enter qusetion number -->
+          <div class="form-group col-lg-8">
+            <label for="qnumber">Question Number</label><br>
+            <input class="form-control" id="qnumber" name="qnumber" type="number" placeholder="Enter Number" min="1" max="20" required>
+            <script>
+              document.querySelector("input[type=number]")
+              .oninput = e => console.log(new Date(e.target.valueAsNumber, 0, 1))
+            </script>
+          </div>
+        </div>
+
+        <div class="row">
+          <!-- input for answer -->
+          <div class="form-group col-lg-8">
+            <label for="answer">Answer</label><br>
+            <input class="form-control" type="text" name="answer" placeholder="Enter Answer" required>
+          </div>
+        </div>
+
+        <div class="row">
+          <!-- select/type year of publication -->
+          <div class="form-group col-lg-8">
+            <label for="year">Publication Year</label><br>
+            <select class="form-control" name="year" required>
+              <?php
+                $year_sql = "SELECT * FROM year ORDER BY yearID DESC";
+                $year_qry = mysqli_query($dbconnect, $year_sql);
+                $year_aa = mysqli_fetch_assoc($year_qry);
+
+                do {
+
+                  $yearID = $year_aa['yearID'];
+                  $name = $year_aa['name'];
+
+                  echo " <option value='$yearID'>$name</option>";
+
+                } while ($year_aa = mysqli_fetch_assoc($year_qry));
+                ?>
+            </select>
+          </div>
+        </div>
+
+        <div class="row">
+          <!-- select/type year level -->
+          <div class="form-group col-lg-8">
+            <label for="level">Year Level</label><br>
+            <select name="level" class="form-control" required>
+              <?php
+              $level_sql = "SELECT * FROM level";
+              $level_qry = mysqli_query($dbconnect, $level_sql);
+              $level_aa = mysqli_fetch_assoc($level_qry);
 
               do {
 
-                $yearID = $year_aa['yearID'];
-                $name = $year_aa['name'];
+                $levelID = $level_aa['levelID'];
+                $name = $level_aa['name'];
 
-                echo " <option value='$yearID'>$name</option>";
+                echo " <option value='$levelID'>Year $name</option>";
 
-              } while ($year_aa = mysqli_fetch_assoc($year_qry));
+              } while ($level_aa = mysqli_fetch_assoc($level_qry));
               ?>
-          </select>
-        </div>
-
-        <!-- select/type year level -->
-        <div class="form-group col-lg-1">
-          <label for="level">Year Level</label><br>
-          <select name="level" class="form-control" required>
-            <?php
-            $level_sql = "SELECT * FROM level";
-            $level_qry = mysqli_query($dbconnect, $level_sql);
-            $level_aa = mysqli_fetch_assoc($level_qry);
-
-            do {
-
-              $levelID = $level_aa['levelID'];
-              $name = $level_aa['name'];
-
-              echo " <option value='$levelID'>Year $name</option>";
-
-            } while ($level_aa = mysqli_fetch_assoc($level_qry));
-            ?>
-          </select>
+            </select>
+          </div>
         </div>
       </div>
 
-      <!-- select tags -->
-      <div class="form-group col-lg-2">
-            <?php
-              $tag_sql = "SELECT * FROM tag";
-              $tag_qry = mysqli_query($dbconnect, $tag_sql);
-              $tag_aa = mysqli_fetch_assoc($tag_qry);
+      <div class="col-6">
+        <!-- select tags -->
+        <div class="form-group col">
+              <?php
+                $tag_sql = "SELECT * FROM tag";
+                $tag_qry = mysqli_query($dbconnect, $tag_sql);
+                $tag_aa = mysqli_fetch_assoc($tag_qry);
 
-              do {
+                do {
 
-                $tagID = $tag_aa['tagID'];
-                $name = $tag_aa['name'];
+                  $tagID = $tag_aa['tagID'];
+                  $name = $tag_aa['name'];
 
-                echo "
-                <div class='form-check'>
-                  <input class='form-check-input' name='tag[]' type='checkbox' value='$tagID' id='checkbox_$tagID'>
-                  <label class='form-check-label' for='checkbox_$tagID'>
-                  $name
-                  </label>
-                </div>";
+                  echo "
+                  <div class='form-check'>
+                    <input class='form-check-input' name='tag[]' type='checkbox' value='$tagID' id='checkbox_$tagID'>
+                    <label class='form-check-label' for='checkbox_$tagID'>
+                    $name
+                    </label>
+                  </div>";
 
-              } while ($tag_aa = mysqli_fetch_assoc($tag_qry));
-            ?>
+                } while ($tag_aa = mysqli_fetch_assoc($tag_qry));
+              ?>
+        </div>
       </div>
-
+    </div>
 
     <div class="row">
       <div class="col-3">
