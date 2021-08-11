58cbc9ca8d6db343df364a6990970e43fecb5c98
mko2302 <mko2302@stacmail.net>
2021-08-11 13:40:36 +1200

    Create entertag.php
    add tags to database, needs error catching

---
+++ website/entertag.php
@@ -0,0 +1,19 @@
+<?php
+  $tag_name = $_POST['tag_name'];
+
+  $check_qry = "SELECT * FROM tag WHERE name LIKE '$tag_name'";
+
+  $check_sql = mysqli_query($dbconnect, $check_qry);
+  if (mysqli_num_rows($check_sql) == 0) {
+    // enter tags into Database
+    $insert_sql = "INSERT INTO tag (name) VALUES ($tag_name)";
+    // sends sql query to data base
+    $insert_qry = mysqli_query($dbconnect, $insert_sql);
+    header("index.php?page=adminpanel&tab=dbsettings&status=tagsuccess");
+  } else {
+    header("index.php?page=adminpanel&tab=dbsettings&status=duplicatetag");
+  }
+
+
+
+ ?>
