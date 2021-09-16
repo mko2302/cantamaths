e437b6ede351168d48e0d4113eb2690d7222935d
mko2302 <mko2302@stacmail.net>
2021-09-17 08:08:24 +1200

    Update addquestion.php
    Layout/styling changes

--- website/addquestion.php
+++ website/addquestion.php
@@ -6,6 +6,8 @@ if (!isset($_SESSION['admin'])) {
 
   include("status.php")
   ?>
+<div class="admin-add-container">
+  <h2>Add Question</h2><br>
   <!-- form for entering question -->
   <form action="index.php?page=adminpanel&tab=enterquestion" method="post" enctype="multipart/form-data">
     <div class="row">
@@ -34,96 +36,106 @@ if (!isset($_SESSION['admin'])) {
         </div>
 
         <div class="row">
-          <!-- enter qusetion number -->
-          <div class="form-group col-lg-8">
-            <label for="qnumber">Question Number</label><br>
-            <input class="form-control" id="qnumber" name="qnumber" type="number" placeholder="Enter Number" min="1" max="20" required>
-            <script>
-              document.querySelector("input[type=number]")
-              .oninput = e => console.log(new Date(e.target.valueAsNumber, 0, 1))
-            </script>
-          </div>
-        </div>
+          <div class="col">
+            <div class="row">
+              <!-- enter qusetion number -->
+              <div class="form-group col">
+                <label for="qnumber">Question Number</label><br>
+                <input class="form-control" id="qnumber" name="qnumber" type="number" placeholder="Enter Number" min="1" max="20" required>
+                <script>
+                  document.querySelector("input[type=number]")
+                  .oninput = e => console.log(new Date(e.target.valueAsNumber, 0, 1))
+                </script>
+              </div>
+            </div>
 
-        <div class="row">
-          <!-- input for answer -->
-          <div class="form-group col-lg-8">
-            <label for="answer">Answer</label><br>
-            <input class="form-control" type="text" name="answer" placeholder="Enter Answer" required>
-          </div>
-        </div>
+            <div class="row">
+              <!-- input for answer -->
+              <div class="form-group col-lg">
+                <label for="answer">Answer</label><br>
+                <input class="form-control" type="text" name="answer" placeholder="Enter Answer" required>
+              </div>
+            </div>
 
-        <div class="row">
-          <!-- select/type year of publication -->
-          <div class="form-group col-lg-8">
-            <label for="year">Publication Year</label><br>
-            <select class="form-control" name="year" required>
-              <?php
-                $year_sql = "SELECT * FROM year ORDER BY yearID DESC";
-                $year_qry = mysqli_query($dbconnect, $year_sql);
-                $year_aa = mysqli_fetch_assoc($year_qry);
+            <div class="row">
+              <!-- select/type year of publication -->
+              <div class="form-group col">
+                <label for="year">Publication Year</label><br>
+                <select class="form-control" name="year" required>
+                  <?php
+                    $year_sql = "SELECT * FROM year ORDER BY yearID DESC";
+                    $year_qry = mysqli_query($dbconnect, $year_sql);
+                    $year_aa = mysqli_fetch_assoc($year_qry);
+
+                    do {
+
+                      $yearID = $year_aa['yearID'];
+                      $name = $year_aa['yearname'];
+
+                      echo " <option value='$yearID'>$name</option>";
+
+                    } while ($year_aa = mysqli_fetch_assoc($year_qry));
+                    ?>
+                </select>
+              </div>
+          </div>
+
+          <div class="row">
+            <!-- select/type year level -->
+            <div class="form-group col">
+              <label for="level">Year Level</label><br>
+              <select name="level" class="form-control" required>
+                <?php
+                $level_sql = "SELECT * FROM level";
+                $level_qry = mysqli_query($dbconnect, $level_sql);
+                $level_aa = mysqli_fetch_assoc($level_qry);
 
                 do {
 
-                  $yearID = $year_aa['yearID'];
-                  $name = $year_aa['yearname'];
+                  $levelID = $level_aa['levelID'];
+                  $name = $level_aa['levelname'];
 
-                  echo " <option value='$yearID'>$name</option>";
+                  echo " <option value='$levelID'>Year $name</option>";
 
-                } while ($year_aa = mysqli_fetch_assoc($year_qry));
+                } while ($level_aa = mysqli_fetch_assoc($level_qry));
                 ?>
-            </select>
+              </select>
+            </div>
           </div>
-        </div>
-
-        <div class="row">
-          <!-- select/type year level -->
-          <div class="form-group col-lg-8">
-            <label for="level">Year Level</label><br>
-            <select name="level" class="form-control" required>
-              <?php
-              $level_sql = "SELECT * FROM level";
-              $level_qry = mysqli_query($dbconnect, $level_sql);
-              $level_aa = mysqli_fetch_assoc($level_qry);
-
-              do {
-
-                $levelID = $level_aa['levelID'];
-                $name = $level_aa['levelname'];
-
-                echo " <option value='$levelID'>Year $name</option>";
-
-              } while ($level_aa = mysqli_fetch_assoc($level_qry));
-              ?>
-            </select>
           </div>
+
+          <div class="col">
+            <div class="col-6">
+              <!-- select tags -->
+              <div class="form-group col">
+                <label for="">Select Tags</label>
+                    <?php
+                      $tag_sql = "SELECT * FROM tag";
+                      $tag_qry = mysqli_query($dbconnect, $tag_sql);
+                      $tag_aa = mysqli_fetch_assoc($tag_qry);
+
+                      do {
+
+                        $tagID = $tag_aa['tagID'];
+                        $name = $tag_aa['tagname'];
+
+                        echo "
+                        <div class='form-check'>
+                          <input class='form-check-input' name='tag[]' type='checkbox' value='$tagID' id='checkbox_$tagID'>
+                          <label class='form-check-label' for='checkbox_$tagID'>
+                          $name
+                          </label>
+                        </div>";
+
+                      } while ($tag_aa = mysqli_fetch_assoc($tag_qry));
+                    ?>
+              </div>
+            </div>
+          </div>
+
         </div>
-      </div>
 
-      <div class="col-6">
-        <!-- select tags -->
-        <div class="form-group col">
-              <?php
-                $tag_sql = "SELECT * FROM tag";
-                $tag_qry = mysqli_query($dbconnect, $tag_sql);
-                $tag_aa = mysqli_fetch_assoc($tag_qry);
 
-                do {
-
-                  $tagID = $tag_aa['tagID'];
-                  $name = $tag_aa['tagname'];
-
-                  echo "
-                  <div class='form-check'>
-                    <input class='form-check-input' name='tag[]' type='checkbox' value='$tagID' id='checkbox_$tagID'>
-                    <label class='form-check-label' for='checkbox_$tagID'>
-                    $name
-                    </label>
-                  </div>";
-
-                } while ($tag_aa = mysqli_fetch_assoc($tag_qry));
-              ?>
-        </div>
       </div>
     </div>
 
@@ -133,6 +145,8 @@ if (!isset($_SESSION['admin'])) {
       </div>
     </div>
   </form>
+</div>
+
   <?php
 }
 ?>
