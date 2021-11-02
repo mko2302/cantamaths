db01a0b83431b10b2c295a238e021ecaf1765c8d
mko2302 <mko2302@stacmail.net>
2021-11-02 23:07:30 +1300

    Commenting

--- website/addquestion.php
+++ website/addquestion.php
@@ -41,7 +41,9 @@ if (!isset($_SESSION['admin'])) {
               <!-- enter qusetion number -->
               <div class="form-group col">
                 <label for="qnumber">Question Number</label><br>
+                <!-- select question number 1 to 20 -->
                 <input class="form-control" id="qnumber" name="qnumber" type="number" placeholder="Enter Number" min="1" max="20" required>
+                <!-- script to display numbers in selector -->
                 <script>
                   document.querySelector("input[type=number]")
                   .oninput = e => console.log(new Date(e.target.valueAsNumber, 0, 1))
@@ -58,15 +60,19 @@ if (!isset($_SESSION['admin'])) {
             </div>
 
             <div class="row">
-              <!-- select/type year of publication -->
+              <!-- select year of competition -->
               <div class="form-group col">
-                <label for="year">Publication Year</label><br>
+                <label for="year">Competition Year</label><br>
                 <select class="form-control" name="year" required>
                   <?php
+                  // select all years from database
                     $year_sql = "SELECT * FROM year ORDER BY yearID DESC";
+                    // send query to database
                     $year_qry = mysqli_query($dbconnect, $year_sql);
+                    //fetch results as assocative array
                     $year_aa = mysqli_fetch_assoc($year_qry);
 
+                    // display each year as a select option
                     do {
 
                       $yearID = $year_aa['yearID'];
@@ -86,10 +92,14 @@ if (!isset($_SESSION['admin'])) {
               <label for="level">Year Level</label><br>
               <select name="level" class="form-control" required>
                 <?php
+                // select all year levels from database
                 $level_sql = "SELECT * FROM level";
+                // send query to database
                 $level_qry = mysqli_query($dbconnect, $level_sql);
+                // get results as assocative array
                 $level_aa = mysqli_fetch_assoc($level_qry);
 
+                // loop though array with each year as an option in a select
                 do {
 
                   $levelID = $level_aa['levelID'];
@@ -110,15 +120,20 @@ if (!isset($_SESSION['admin'])) {
               <div class="form-group col">
                 <label for="">Select Tags</label>
                     <?php
+                    // select all tags from database
                       $tag_sql = "SELECT * FROM tag";
+                      // send query to database
                       $tag_qry = mysqli_query($dbconnect, $tag_sql);
+                      // returnts results as assocative array
                       $tag_aa = mysqli_fetch_assoc($tag_qry);
 
+                      // loopthough each result in the array
                       do {
 
                         $tagID = $tag_aa['tagID'];
                         $name = $tag_aa['tagname'];
 
+                        // display each tag as a checkbox
                         echo "
                         <div class='form-check'>
                           <input class='form-check-input' name='tag[]' type='checkbox' value='$tagID' id='checkbox_$tagID'>
@@ -140,6 +155,7 @@ if (!isset($_SESSION['admin'])) {
     </div>
 
     <div class="row">
+      <!-- submit button -->
       <div class="col-3">
         <button type="submit" class="btn btn-primary">Add Question</button>
       </div>
--- website/enterquestion.php
+++ website/enterquestion.php
@@ -8,15 +8,17 @@
     $tags = $_POST['tag'];
   }
 
+  //sql query to check for a question with the same details
   $check_sql = "SELECT * FROM question WHERE qnumber LIKE $qnumber AND yearID LIKE '$year' AND levelID LIKE $level";
-
+  //send query to database
   $check_qry = mysqli_query($dbconnect, $check_sql);
 
-  clearstatcache();
-
     if (mysqli_num_rows($check_qry) > 0){
+      // if the question exists in the database (more than 1 row)
+      //send user back to add quesiton, with the error duplicate question
       header("Location:index.php?page=adminpanel&tab=addquestion&status=duplicateq");
     } else {
+      // variable for directory where images are held.
       $target_dir = "questions/";
       //path of where file is uploaded
       $target_file = $target_dir . basename($_FILES["fileToUpload"]["tmp_name"]);
@@ -30,65 +32,77 @@
       if($check !== false) {
         $uploadOk = 1;
       } else {
+        // cancel upload of image and then send user back with error
           $uploadOk = 0;
           header("Location:index.php?page=adminpanel&tab=addquestion&status=nonimage");
         }
 
       // Check if file already exists
       if (file_exists($target_file)) {
+        //if the file exists cancel file upload and send user back with error
         $uploadOk = 0;
         header("Location:index.php?page=adminpanel&tab=addquestion&status=duplicateimage");
       }
 
       // Check file size
-      if ($_FILES["fileToUpload"]["size"] > 7000000) {
+      if ($_FILES["fileToUpload"]["size"] > 10000000) {
+        //if the image is too large, cancel upload and sen user back with error
         echo "Sorry, your file is too large.";
         $uploadOk = 0;
+        header("Location:index.php?page=adminpanel&tab=addquestion&status=largefile");
       }
 
       // change filename
 
+      //select year from table
       $year_sql = "SELECT * FROM year WHERE yearID = $year";
       $year_qry = mysqli_query($dbconnect, $year_sql);
       $year_aa = mysqli_fetch_assoc($year_qry);
+      //set file_year variable of the year sent from the form
       $file_year = $year_aa['yearname'];
 
+      //select level from table
       $level_sql = "SELECT * FROM level WHERE levelID = $level";
       $level_qry = mysqli_query($dbconnect, $level_sql);
       $level_aa = mysqli_fetch_assoc($level_qry);
+      //set file_level variable of the name of year level sent from form
       $file_level = $level_aa['levelname'];
 
       // gets file extenetion
       $file_path = pathinfo($_FILES["fileToUpload"]["name"]);
       $extension = $file_path['extension'];
 
+      //$filename variable is the new file name.
       $filename = $file_year . "-" . $file_level . "-" . $qnumber . "." . $extension;
+      //
       $new_target_file = $target_dir . $filename;
 
       // Check if $uploadOk is set to 0 by an error
       if ($uploadOk == 0) {
-        // header("Location:index.php?page=adminpanel&tab=addquestion&status=error");
+        //if any errors caused uploadOk = 0
+        //send user back with error
+        header("Location:index.php?page=adminpanel&tab=addquestion&status=error");
       // if everything is ok, try to upload file
       } else {
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $new_target_file)) {
           //insert question into database
-
           $insert_sql = "INSERT INTO question (qnumber, filename, answer, yearID, levelID) VALUES ($qnumber, '$filename', '$answer', $year, $level)";
           // sends sql query to data base
           $insert_qry = mysqli_query($dbconnect, $insert_sql);
-
+          //get the questionID of the quetion just inserted
           $lastID = mysqli_insert_id($dbconnect);
 
+          // for each tag in the tag array
           foreach ($tags as $tagID){
+            //insert the question and tag into the junction table
             $addtag_sql = "INSERT INTO questiontag (questionID, tagID) VALUES ($lastID, $tagID)";
-
+            //sends query to database
             $addtag_qry = mysqli_query($dbconnect, $addtag_sql);
           }
-
-          echo "success $filename";
-
+          //send user back to add question page with success alerts
           header("Location:index.php?page=adminpanel&tab=addquestion&status=addsuccess");
         } else {
+            //if any other errors occur, send user back to add question page.
             header("Location:index.php?page=adminpanel&tab=addquestion&status=error");
         }
         }
