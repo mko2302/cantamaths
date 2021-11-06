70c3ee43d5320280feb7609c4cc59a015a8135cb
mko2302 <mko2302@stacmail.net>
2021-11-07 02:35:48 +1300

    Update login.php
    bootstrap styles

--- website/login.php
+++ website/login.php
@@ -3,20 +3,35 @@ if (isset($_SESSION['admin'])) {
   header("Location: index.php?page=adminpanel");
 }
  ?>
+<div class="container center">
+  <div class="login-container">
+    <p class="display-4 p-2">Login</p>
+    <form class="" action="verify.php" method="post">
 
-<div class="container">
-  <p class="display-4 p-2">Login</p>
-  <form class="m-2" action="verify.php" method="post">
-    <div class="form-row">
-      <div class="form-group col-md-6">
-        <label for="username">Username</label>
-        <input type="text" class="form-control" name="username" placeholder="Username">
+      <div class="row mx-2 login-field">
+        <div class="form-group">
+          <label for="username">Username</label>
+          <input type="text" class="form-control" name="username" placeholder="Username">
+        </div>
       </div>
-      <div class="form-group col-md-6">
-        <label for="password">Password</label>
-        <input type="password" class="form-control" name="password" placeholder="Password">
+
+      <div class="row mx-2 login-field">
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="password" class="form-control" name="password" placeholder="Password">
+        </div>
       </div>
-    </div>
-  <button type="submit" class="btn btn-primary">Login</button>
-</form>
+
+      <div class="row mx-2">
+          <?php include("status.php") ?>
+      </div>
+
+
+      <div class="form-group m-2">
+        <button type="submit" class="btn btn-primary">Login</button>
+      </div>
+
+    </form>
+  </div>
+
 </div>
