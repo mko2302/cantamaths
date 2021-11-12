1b09c88b848724b524a5dfc6736ac8c595d0d681
tpa4077 <60764698+tpa4077@users.noreply.github.com>
2021-11-12 19:55:17 +1300

    Fixed red circle border

--- website/breadcrumbs.php
+++ website/breadcrumbs.php
@@ -1,8 +1,8 @@
 <?php if ($page == "option-select") { ?>
   <nav aria-label="breadcrumb">
     <ol class="breadcrumb justify-content-center">
-      <li class="breadcrumb-item"><a class="active-1-a" href="index.php?"><span class="circle active">1</span>Home</a></li>
-      <li class="breadcrumb-item"><a class="active-1-a" href=""><span class="circle active">2</span>Option Select</a></li>
+      <li class="breadcrumb-item"><a class="active-1-a" href="index.php?"><span class="circle active-bread">1</span>Home</a></li>
+      <li class="breadcrumb-item"><a class="active-1-a" href=""><span class="circle active-bread">2</span>Option Select</a></li>
       <li class="breadcrumb-item"><span class="circle inactive">3</span>Select</li>
       <li class="breadcrumb-item"><span class="circle inactive">4</span>Print</li>
     </ol>
@@ -12,9 +12,9 @@
 <?php if ($page == "select-index") { ?>
   <nav aria-label="breadcrumb">
     <ol class="breadcrumb justify-content-center">
-      <li class="breadcrumb-item"><a class="active-1-a" href="index.php?"><span class="circle active">1</span>Home</a></li>
-      <li class="breadcrumb-item"><a class="active-1-a" href="index.php?page=option-select"><span class="circle active">2</span>Option Select</a></li>
-      <li class="breadcrumb-item"><a class="active-1-a" href="#"><span class="circle active">3</span>Select</a></li>
+      <li class="breadcrumb-item"><a class="active-1-a" href="index.php?"><span class="circle active-bread">1</span>Home</a></li>
+      <li class="breadcrumb-item"><a class="active-1-a" href="index.php?page=option-select"><span class="circle active-bread">2</span>Option Select</a></li>
+      <li class="breadcrumb-item"><a class="active-1-a" href="#"><span class="circle active-bread">3</span>Select</a></li>
       <li class="breadcrumb-item"><span class="circle inactive">4</span>Print</li>
     </ol>
   </nav>
@@ -23,14 +23,14 @@
 <?php if ($page == "print") { ?>
   <nav aria-label="breadcrumb">
     <ol class="breadcrumb justify-content-center">
-      <li class="breadcrumb-item"><a class="active-1-a" href="index.php?"><span class="circle active">1</span>Home</a></li>
-      <li class="breadcrumb-item"><a class="active-1-a" href="index.php?page=option-select"><span class="circle active">2</span>Option Select</a></li>
+      <li class="breadcrumb-item"><a class="active-1-a" href="index.php?"><span class="circle active-bread">1</span>Home</a></li>
+      <li class="breadcrumb-item"><a class="active-1-a" href="index.php?page=option-select"><span class="circle active-bread">2</span>Option Select</a></li>
       <?php if (in_array("custom", $_SESSION['alex!'])) {?>
-        <li class="breadcrumb-item"><a class="active-1-a" href="index.php?page=select-index&select=custom"><span class="circle active">3</span>Select</a></li>
+        <li class="breadcrumb-item"><a class="active-1-a" href="index.php?page=select-index&select=custom"><span class="circle active-bread">3</span>Select</a></li>
       <?php } elseif (in_array("pastpapers", $_SESSION['alex!'])) { ?>
-        <li class="breadcrumb-item"><a class="active-1-a" href="index.php?page=select-index&select=pastpapers"><span class="circle active">3</span>Select</a></li>
+        <li class="breadcrumb-item"><a class="active-1-a" href="index.php?page=select-index&select=pastpapers"><span class="circle active-bread">3</span>Select</a></li>
       <?php } ?>
-      <li class="breadcrumb-item"><a class="active-1-a" href="#"><span class="circle active">4</span>Print</a></li>
+      <li class="breadcrumb-item"><a class="active-1-a" href="#"><span class="circle active-bread">4</span>Print</a></li>
     </ol>
   </nav>
 <?php } ?>
